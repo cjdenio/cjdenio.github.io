7f153ca8461c90eff6d46ec713ab30ef3da63f5b
Caleb Denio <cjdenio44@gmail.com>
2020-02-15 01:52:53 -0500

    Changes

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 $(document).scroll(function(e){
-    if($(document).scrollTop() > 500){
+    if($(document).scrollTop() > $('#title').offset().top + $('#title').height()){
         $('#header').removeClass("hidden")
     }
     else{
         $('#header').addClass("hidden")
     }
 })
+
+var easter_egg = new Konami(function(){
+    $('#easter_egg').removeClass("hidden")
+})
+
+$('#easter-egg-back').click(() => {$('#easter_egg').addClass("hidden")})
